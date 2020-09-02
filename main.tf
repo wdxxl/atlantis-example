e6ea7199aaff50fe4d0c69515748585c4f49f920
wdxxl <wdxxldeveloper@gmail.com>
2020-09-02 17:28:06 +0800

    ypdate for real terraform

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-resource "null_resource" "example" {}
+provider "aws" {
+  version                 = "~> 3.0"
+  region                  = "cn-northwest-1"
+  shared_credentials_file = "/Users/wdxxl/.aws/credentials"
+  profile                 = "meican-sandbox"
+}
+
+resource "aws_db_instance" "wdxxl" {
+  name                 = "wdxxl"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "PostgreSQL"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+}
+
